5b802d4c802fc68d4a0f838f18a4336f048e8af6
Suresh Koppisetti <suresh.koppisetti@gmail.com>
2017-07-29 19:58:23 -0400

    Lesson 1.5 Complete

---
+++ src/Unit-1/DoThis/FileValidatorActor.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using Akka.Actor;
+
+namespace WinTail
+{
+    /// <summary>
+    /// Actor that validates use input and signals result to others
+    /// </summary>
+    public class FileValidatorActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+        private readonly IActorRef _tailCoordinatorActor;
+
+        public FileValidatorActor(IActorRef consoleWriterActor, IActorRef tailCoordinatorActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+            _tailCoordinatorActor = tailCoordinatorActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (string.IsNullOrEmpty(msg))
+            {
+                // signal  that the user needs to pass an input
+                _consoleWriterActor.Tell(new Messages.NullInputError("Input was blank. Please try again.\n"));
+
+                // tell sender to continue doin its thing
+                Sender.Tell(new Messages.ContinueProcessing());
+            }
+            else
+            {
+                var valid = IsFileUri(msg);
+                if (valid)
+                {
+                    // send success to the console writer
+                    _consoleWriterActor.Tell(new Messages.InputSuccess($"Starting processing for {msg}"));
+
+                    // start the coordinator
+                    _tailCoordinatorActor.Tell(new TailCoordinatorActor.StartTail(msg, _consoleWriterActor));
+                }
+                else
+                {
+                    // signal the inout was bad
+                    _consoleWriterActor.Tell(new Messages.ValidationError($"{msg} is not an existing URI on disk."));
+
+                    // tell sender to continue
+                    Sender.Tell(new Messages.ContinueProcessing());
+                }
+            }
+            // tell sender to continue doing its thing
+            // (whaterever that may be, this actor doesn't care
+            Sender.Tell(new Messages.ContinueProcessing());
+        }
+
+        private static bool IsFileUri(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+}
---
+++ src/Unit-1/DoThis/TailActor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Akka.Actor;
+
+namespace WinTail
+{
+    public class TailActor : UntypedActor
+    {
+        #region Message types
+        public class FileWrite
+        {
+            public FileWrite(string fileName)
+            {
+                FileName = fileName;
+            }
+
+            public string FileName { get; private set; }
+        }
+
+        public class FileError
+        {
+            public FileError(string fileName, string reason)
+            {
+                FileName = fileName;
+                Reason = reason;
+            }
+
+            public string FileName { get; private set; }
+            public string Reason { get; private set; }
+        }
+
+        public class InitialRead
+        {
+            public InitialRead(string fileName, string text)
+            {
+                FileName = fileName;
+                Text = text;
+            }
+
+            public string Text { get; private set; }
+            public string FileName { get; private set; }
+        }
+        #endregion
+
+        private readonly string _filePath;
+        private readonly IActorRef _reporterActor;
+        private readonly FileObserver _observer;
+        private readonly Stream _fileStream;
+        private readonly StreamReader _fileStreamReader;
+
+        public TailActor(IActorRef reporterActor, string filePath)
+        {
+            _reporterActor = reporterActor;
+            _filePath = filePath;
+
+            _observer = new FileObserver(Self, Path.GetFullPath(_filePath));
+            _observer.Start();
+
+            _fileStream = new FileStream(Path.GetFullPath(_filePath), FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            _fileStreamReader = new StreamReader(_fileStream, Encoding.UTF8);
+
+            var text = _fileStreamReader.ReadToEnd();
+            Self.Tell(new InitialRead(_filePath, text));
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if(message is FileWrite)
+            {
+                var text = _fileStreamReader.ReadToEnd();
+                if (!string.IsNullOrEmpty(text))
+                {
+                    _reporterActor.Tell(text);
+                }
+            }
+            else if(message is FileError)
+            {
+                var fe = message as FileError;
+                _reporterActor.Tell($"Tail error: {fe.Reason}");
+            }
+            else if(message is InitialRead)
+            {
+                var ir = message as InitialRead;
+                _reporterActor.Tell(ir.Text);
+            }
+        }
+    }
+}
